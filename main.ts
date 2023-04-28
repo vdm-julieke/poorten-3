4c82ebe0db7da73ddc29d3402bee283cb2909237
vdm-julieke <124454367+vdm-julieke@users.noreply.github.com>
2023-04-28 09:47:27 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right) && !(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left))) {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) && !(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right))) {
         basic.showNumber(1)
     } else {
         basic.showNumber(0)
